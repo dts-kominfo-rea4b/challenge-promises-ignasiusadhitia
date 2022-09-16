fdc2c92b51049dadb35cf69f7d0917e75e4358b9
Ignasius Yuda Adhitia <ignasius.yuda.adhitia@gmail.com>
2022-09-16 13:36:30 +0700

    completed promiseOutput function, challenge solved!

--- promise.js
+++ promise.js
@@ -1,7 +1,25 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (reaksi) => {
+  try {
+    const reaksiSetelahMenontonDiTheaterIXX = await promiseTheaterIXX();
+    const reaksiSetelahMenontonDiTheaterVGC = await promiseTheaterVGC();
+
+    const reaksiSetelahMenontonDiKeduaTheater = [
+      ...reaksiSetelahMenontonDiTheaterIXX,
+      ...reaksiSetelahMenontonDiTheaterVGC,
+    ];
+
+    const hitungJumlahReaksi = reaksiSetelahMenontonDiKeduaTheater.filter(
+      (hasilMenonton) => hasilMenonton.hasil === reaksi
+    ).length;
+
+    return hitungJumlahReaksi;
+  } catch (error) {
+    return error.message + "!";
+  }
+};
 
 module.exports = {
   promiseOutput,
